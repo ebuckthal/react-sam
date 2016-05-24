dfeb839d478598d75590e3770a0076d67a8394c3
Eric Buckthal <ebuckthal@gmail.com>
2016-05-23 21:46:08 -0700

    works better with phone resolution

--- src/main.js
+++ src/main.js
@@ -11,20 +11,44 @@ import Store from './store';
 
 ReactDOM.render(
    <Provider store={ Store() }>
-      <Router history={ browserHistory } >
-         <Route path="/" component={ App }>
-            <Route path="/dualscape" component={ Dualscape } />
-            <Route path="/17th-street" component={ SeventeenthStreet } />
-            <Route path="/collective-authorship" component={ CollectiveAuthorship } />
-         </Route>
-      </Router>
+   <Router history={ browserHistory } >
+   <Route path="/" component={ App }>
+   <Route path="/dualscape" component={ Dualscape } />
+   <Route path="/17th-street" component={ SeventeenthStreet } />
+   <Route path="/collective-authorship" component={ CollectiveAuthorship } />
+   </Route>
+   </Router>
    </Provider>,
    document.getElementById('root')
 );
 
-var i = 0;
-var bg_colors = ['magenta', 'yellow', 'cyan'];
-var bg_interval = window.setInterval(function() { 
-      i = (++i % bg_colors.length);
-      document.body.style.backgroundColor = bg_colors[i];
-}, 250);
+
+
+var urlParams;
+(window.onpopstate = () => {
+         var match,
+         pl     = /\+/g,  // Regex for replacing addition symbol with a space
+         search = /([^&=]+)=?([^&]*)/g,
+         decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+         query  = window.location.search.substring(1);
+
+         urlParams = {}
+         while (match = search.exec(query)) {
+            urlParams[decode(match[1])] = decode(match[2]);
+         }
+   }
+)();
+
+var setColorFlashingBackground = () => {
+   var i = 0;
+   var bg_colors = ['magenta', 'yellow', 'cyan'];
+   var bg_interval = window.setInterval(function() { 
+         i = (++i % bg_colors.length);
+         document.body.style.backgroundColor = bg_colors[i];
+   }, 250);
+}
+
+if(!urlParams.debug) {
+   setColorFlashingBackground();
+}
+
